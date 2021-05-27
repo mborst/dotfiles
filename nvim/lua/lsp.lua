e66a12631982a6747592215ba255791c866107f6
Michael Borst <mborst@users.noreply.github.com>
2021-05-27 14:20:01 +0200

    Add custom pyls settings

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -1,5 +1,32 @@
 local nvim_lsp = require('lspconfig')
 
+settings = {
+  pyls = {
+    pyls = {
+      configurationSources = {"flake8"},
+      plugins = {
+        jedi_completion = {enabled = true},
+        jedi_hover = {enabled = true},
+        jedi_references = {enabled = true},
+        jedi_signature_help = {enabled = true},
+        jedi_symbols = {enabled = true, all_scopes = true},
+        pycodestyle = {enabled = false},
+        flake8 = {
+          enabled = true,
+        },
+        mypy = {enabled = false},
+        isort = {enabled = false},
+        yapf = {enabled = false},
+        pylint = {enabled = false},
+        pydocstyle = {enabled = false},
+        mccabe = {enabled = false},
+        preload = {enabled = false},
+        rope_completion = {enabled = false}
+      }
+    }
+  }
+}
+
 local on_attach = function(client, bufnr)
   require('completion').on_attach()
 
@@ -52,5 +79,6 @@ local servers = { 'gopls', 'pyls' }
 for _, lsp in ipairs(servers) do
   nvim_lsp[lsp].setup {
     on_attach = on_attach,
+    settings = settings[lsp]
   }
 end
