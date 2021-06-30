24e89c861dae08dae1e35f1f13a6ba2c4a171be0
Michael Borst <mborst@users.noreply.github.com>
2021-06-30 23:26:05 +0200

    [vim] enable lsp virtual hints

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -29,7 +29,7 @@ vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
     vim.lsp.diagnostic.on_publish_diagnostics,
     {
         underline = false,
-        virtual_text = false,
+        virtual_text = true,
         update_in_insert = false
     }
 )
