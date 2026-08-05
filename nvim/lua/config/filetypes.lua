5c025aeaea3d4b10b1d9a4470ab04fda4142af9e
Michael Borst <mborst@users.noreply.github.com>
2026-08-05 10:54:54 +0200

    [git] Disable smartindent in gitcommit
    It's not useful, and adds indent add the beginning of lines randomly.

--- nvim/lua/config/filetypes.lua
+++ nvim/lua/config/filetypes.lua
@@ -25,6 +25,7 @@ end
 
 ftset({ "bzl" },             { tabstop = 4, shiftwidth = 4 })
 ftset({ "gitconfig", "go" }, { expandtab = false })
+ftset({ "gitcommit"},        { smartindent = false })
 ftset({ "make" },            { expandtab = false, tabstop = 4, shiftwidth = 4, softtabstop = 0 })
 -- macOS crontab rejects edits if backup files are created
 ftset({ "crontab" },         { backup = false, writebackup = false })
