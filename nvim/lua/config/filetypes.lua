f243d90814ba7f4ab8275dccb34219e82a0bcdea
Michael Borst <mborst@users.noreply.github.com>
2026-08-12 09:17:18 +0200

    [vim] Set markdown textwidth to 120
    80 is a bit dated.

--- nvim/lua/config/filetypes.lua
+++ nvim/lua/config/filetypes.lua
@@ -27,5 +27,6 @@ ftset({ "bzl" },             { tabstop = 4, shiftwidth = 4 })
 ftset({ "gitconfig", "go" }, { expandtab = false })
 ftset({ "gitcommit"},        { smartindent = false })
 ftset({ "make" },            { expandtab = false, tabstop = 4, shiftwidth = 4, softtabstop = 0 })
+ftset({ "markdown" },         { textwidth = 120 })
 -- macOS crontab rejects edits if backup files are created
 ftset({ "crontab" },         { backup = false, writebackup = false })
